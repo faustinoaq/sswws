7dc0be8b039fe4458d67d444b1477cc7843d40b0
faustinoaq <faustinoaq@users.noreply.github.com>
2023-03-16 17:15:11 -0500

    Update sswws.cr

--- src/sswws.cr
+++ src/sswws.cr
@@ -1,17 +1,16 @@
 # TODO: Write more documentation for `Sswws`
 # @faustinoaq
 require "http"
-require "option_parser"
 require "./sswws/art"
 require "./sswws/options"
 
 module Sswws
   options
-  handlers = [HTTP::LogHandler.new, HTTP::ErrorHandler.new, HTTP::StaticFileHandler.new(@@directory)]
+  handlers = [HTTP::LogHandler.new, HTTP::ErrorHandler.new, HTTP::StaticFileHandler.new(@@dir)]
   server = HTTP::Server.new(handlers)
   address = server.bind_tcp(@@ip, @@port)
   puts ART
-  puts "SSWWS is listening on http://#{address} and serving files in current path ."
+  puts "SSWWS is listening on http://#{address} and serving files"
   puts "Any issues? Report them on github.com/faustinoaq/sswws/issues"
   server.listen
 end
